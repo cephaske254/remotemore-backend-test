ce53a79e563f9dcf45d2647603eb4909bfdb9f50
cephaske254 <cephaske254@gmail.com>
2023-08-19 22:50:33 +0300

    revert app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
- 
+
 const app = express();
 const routes = require("./routes");
 
@@ -17,8 +17,6 @@ app.use(cors())
 app.use("/", routes);
 
 const port = Number(process.env.PORT ?? 5000);
-
 app.listen(port, () => {
   console.log("Server is running on port", port);
 });
-
