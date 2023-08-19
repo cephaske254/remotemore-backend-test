8c94d8e9cadbe62183040c587eb202abb74563da
cephaske254 <cephaske254@gmail.com>
2023-08-19 22:59:49 +0300

    change node from @now to @vercel

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
+ 
 const app = express();
 const routes = require("./routes");
 
@@ -17,6 +17,9 @@ app.use(cors())
 app.use("/", routes);
 
 const port = Number(process.env.PORT ?? 5000);
+
 app.listen(port, () => {
   console.log("Server is running on port", port);
 });
+
+module.exports = app;
